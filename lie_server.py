ce62274be29d3e8bb6cd84921c9a43ee6eeb719a
Leo <leooel97895750@gmail.com>
2019-10-17 00:27:39 +0800

    del dict by destroy button color 1 to 1 instant chat

--- lie_server.py
+++ lie_server.py
@@ -23,6 +23,7 @@ def subThread(connection, myfileno):
     
     # 接收使用者傳來的暱稱
     nickname = connection.recv(1024).decode('UTF-8')
+    connection.sendall(str(myfileno).encode('UTF-8'))
     
     myNameDict[myfileno] = nickname
     mySockDict[myfileno] = connection
@@ -33,15 +34,23 @@ def subThread(connection, myfileno):
     while True:
         # 接收使用者傳來的編號和訊息
         msg = connection.recv(65536).decode('UTF-8')
-        msgInfo = msg.split()
-        msgType = int(msgInfo[0])
-        msgText = msg[msg.index(" ")+1:]
+        msgType = msg.split()
         
         # 在server上顯示的系統訊息
         print(myNameDict[myfileno], ':', msg)
         
+        if msgType[0] == "roomOpen":
+            print(msgType[1])
+            mySockDict[int(msgType[1].split(':')[1])].sendall(msg.encode('UTF-8'))
+            
         # 傳送給特定使用者的私人訊息
-        mySockDict[msgType].sendall(("messagePass "+msgText).encode('UTF-8'))
+        elif msgType[0] == "messagePass":
+            mySockDict[int(msgType[1].split(':')[1])].sendall(msg.encode('UTF-8'))
+        
+        elif msgType[0] == "deleteDict":
+            del myNameDict[int(msgType[1])]
+            del mySockDict[int(msgType[1])]
+            updateDict()
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- tkinterTest.py
+++ tkinterTest.py
@@ -12,11 +12,18 @@ import json
 rooms = {}
 userDict = {}
 
-def sendThread():    
-    while True:
-        # 輸入編號已決定要穿送訊息的對象(用空白隔開)
-        myMsg = input()
-        sock.sendall(myMsg.encode('UTF-8'))
+def rootClose():
+    global fileno
+    sock.sendall(("deleteDict "+fileno).encode('UTF-8'))
+    sock.close()
+    root.destroy()
+
+# 建立聊天室類別，紀錄獨立各別物件
+class RoomItems():
+    def __init__(self, room, message, entry):
+        self.room = room
+        self.message = message
+        self.entry = entry
 
 def recvThread():
     global userDict, rooms
@@ -28,25 +35,30 @@ def recvThread():
         
         # 其他使用者的訊息
         if msgType[0] == 'messagePass': 
-            print(msgText)
+            global rooms
+    
+            # 顯示對方的訊息
+            rooms[msgType[2]].message.insert(END, msgType[3]+'\n', "tagOther")
+            rooms[msgType[2]].message.see(END)
         
         # 被通知開啟聊天室
         elif msgType[0] == 'roomOpen':
             room = Toplevel()
-            room.title(name)
+            room.title(msgType[2])
+            room.configure(background='#AAFFAA')
             
-            message = Text(room, exportselection=False)
-            message.pack()
+            message = Text(room, exportselection=False, width=30, font=('微軟正黑體', 10, 'bold'))
+            message.grid(row=0, column=0, padx=5, pady=5)
             message.tag_config("tagOther", foreground="black", justify=LEFT)
-            message.tag_config("tagMe", foreground="black", justify=RIGHT)
+            message.tag_config("tagMe", foreground="black", background="#DDFFDD", justify=RIGHT)
             
-            entry = Entry(room)
-            entry.pack()
-            entry.bind("<Return>", handlerAdaptor(sendMessage,name))
+            entry = Entry(room, width=30, font=('微軟正黑體', 10, 'bold'))
+            entry.grid(row=1, column=0, sticky=E, padx=5, pady=5)
+            entry.bind("<Return>", handlerAdaptor(sendMessage, msgType[2], msgType[1]))
         
             # 建立物件
             NewRoom = RoomItems(room, message, entry)
-            rooms[name] = NewRoom
+            rooms[msgType[2]] = NewRoom
         
         # 更新使用者列表
         else:
@@ -54,41 +66,44 @@ def recvThread():
             try:
                 updateUserList()
             except:
-                print('updateUserList Error')
+                # server產生thread時，第一次更新UserList會因為listbox尚未定義而進入except
+                pass
 
 def updateUserList():
-    global userDict
+    global userDict, nickname, fileno
     
     # 刪除原本的lsitbox並更新
     listbox.delete(0, END)
+    bcolor = 0
     for k, v in userDict.items():
-        userID = v + " : " + str(k)
-        listbox.insert(END, userID)
-
-# 建立聊天室類別，紀錄獨立各別物件
-class RoomItems():
-    def __init__(self, room, message, entry):
-        self.room = room
-        self.message = message
-        self.entry = entry
+        if k != fileno or v != nickname:
+            bcolor = bcolor + 1
+            userID = v + ":" + str(k)
+            listbox.insert(END, userID)
+            if bcolor%2 == 1:
+                listbox.itemconfig(END, bg="#DDFFDD")
+            else:
+                listbox.itemconfig(END, bg="#FFFFFF")
 
 # binding enter，傳送entry中的訊息到對應的聊天室
-def sendMessage(event, name):
+def sendMessage(event, roomName, myName):
     global rooms
     
     # 顯示於自己的視窗
-    text = rooms[name].entry.get()
-    rooms[name].entry.delete(0, END)
-    rooms[name].message.insert(END, text+'\n', "tagMe")
-    rooms[name].message.see(END)
-    
-    # 傳送至對方的聊天室
+    text = rooms[roomName].entry.get()
+    if text != "" and text != " ":
+        rooms[roomName].entry.delete(0, END)
+        rooms[roomName].message.insert(END, text+'\n', "tagMe")
+        rooms[roomName].message.see(END)
+        
+        # 傳送至對方的聊天室
+        sock.sendall(("messagePass "+roomName+" "+myName+" "+text).encode('UTF-8'))
 
-def handlerAdaptor(fun, name):
-    return lambda event,fun=fun,name=name: fun(event, name)
+def handlerAdaptor(fun, roomName, myName):
+    return lambda event, fun=fun, roomName=roomName, myName=myName: fun(event, roomName, myName)
 
 # 初始化聊天室
-def chat():
+def chat(nickname, fileno):
     global rooms
     
     # 建立自己對應的獨立聊天室
@@ -96,22 +111,23 @@ def chat():
     name = listbox.get(indexes[0])
     room = Toplevel()
     room.title(name)
+    room.configure(background='#AAFFAA')
     
-    message = Text(room, exportselection=False)
-    message.pack()
+    message = Text(room, exportselection=False, width=30, font=('微軟正黑體', 10, 'bold'))
+    message.grid(row=0, column=0, padx=5, pady=5)
     message.tag_config("tagOther", foreground="black", justify=LEFT)
-    message.tag_config("tagMe", foreground="black", justify=RIGHT)
+    message.tag_config("tagMe", foreground="black", background="#DDFFDD", justify=RIGHT)
     
-    entry = Entry(room)
-    entry.pack()
-    entry.bind("<Return>", handlerAdaptor(sendMessage,name))
+    entry = Entry(room, width=30, font=('微軟正黑體', 10, 'bold'))
+    entry.grid(row=1, column=0, sticky=E, padx=5, pady=5)
+    entry.bind("<Return>", handlerAdaptor(sendMessage, name, (nickname+":"+fileno)))
 
     # 建立物件
     NewRoom = RoomItems(room, message, entry)
     rooms[name] = NewRoom
     
     # 通知對方開啟聊天室
-    sock.sendall("roomOpen".encode('UTF-8'))
+    sock.sendall(("roomOpen "+name+" "+nickname+":"+fileno).encode('UTF-8'))
 
 # 主程式開始
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -120,31 +136,29 @@ sock.connect(('127.0.0.1', 12345))
 # 輸入暱稱
 nickname = input("請輸入你的暱稱...\n")
 sock.sendall(nickname.encode('UTF-8'))
+fileno = sock.recv(1024).decode('UTF-8')
 
-th1 = threading.Thread(target=sendThread)
-th2 = threading.Thread(target=recvThread)
+th1 = threading.Thread(target=recvThread)
 th1.setDaemon(True)
-th2.setDaemon(True)
 th1.start()
-th2.start()
 
 # 圖形介面布局
 root = Tk()
+root.title("Line")
+root.configure(background='#AAFFAA')
+root.protocol('WM_DELETE_WINDOW', rootClose)
 
 # 標題
-label = Label(root, text="Line")
-label.pack()
+label = Label(root, text="使用者(線上)", width=30, bg='#AAFFAA', font=('微軟正黑體', 10, 'bold'))
+label.grid(row=0, column=0, pady=5)
 
 # 使用者列表
-scrollbar = Scrollbar(root)
-scrollbar.pack(side=RIGHT, fill=Y)
-listbox = Listbox(root, yscrollcommand=scrollbar.set)
-listbox.pack()
-scrollbar.config(command=listbox.yview)
+listbox = Listbox(root, width=30, height=15, font=('微軟正黑體', 15, 'bold'))
+listbox.grid(row=1, column=0, padx=5)
 
 # 開啟聊天室按鍵
-button = Button(root, text="開始聊天", command=chat)
-button.pack()
+button = Button(root, text="開始聊天", width=15, relief=GROOVE, fg="white", bg="green3", font=('微軟正黑體', 10, 'bold'), command=lambda:chat(nickname, fileno))
+button.grid(row=2, column=0, pady=5)
 
 updateUserList()
 
